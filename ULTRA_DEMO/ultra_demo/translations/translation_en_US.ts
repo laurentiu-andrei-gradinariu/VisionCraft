ff42e7ad925e9b547d540881d7c18278e076925d
laurentiu-andrei-gradinariu <laurentiu-andrei.gradinariu@student.tuiasi.ro>
2025-11-12 16:24:29 +0200

    Versiune_Intermediara

---
+++ ULTRA_DEMO/ultra_demo/translations/translation_en_US.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Alex, you are a big IDIOT</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Alex, you are a big IDIOT</translation>
+        </message>
+        <message>
+            <source>Stefan, you are a big IDIOT</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Stefan, you are a big IDIOT</translation>
+        </message>
+        <message>
+            <source>Grosu esti un idiot</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Grosu esti un idiot</translation>
+        </message>
+    </context>
+</TS>
